bed6674075763ac16aed5c38bd9bd89e765744b0
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 14:28:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
  		{
@@ -76,13 +75,6 @@ def adding_matz
         }
      }
   programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
-#  {:known_for => "Ruby",:languages => ["LISP, C"]}
-  programmer_hash
-  # add the following information to the top level of the programmer hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
 
 end
 
